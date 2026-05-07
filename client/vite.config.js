5c65f56d3ea7d3d1a46f99a955bf5ee5b4ba1f07
Morg-Johnson <mnjohnson@csuchico.edu>
2026-05-06 23:48:09 -0700

    fix something

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/cravingscompass/',
+  base: process.env.VITE_BASE_PATH || '/',
   server: {
     watch: {
       usePolling: true,
